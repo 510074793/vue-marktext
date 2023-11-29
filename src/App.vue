35ca64fea8984616078930dfd653f0c4315d5f89
gshu <gshu@evomotion.com>
2023-11-29 19:10:32 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <script setup>
 import zhCn from "element-plus/es/locale/lang/zh-cn"
+// import { onBeforeMount } from "vue";
 
-window.onload = function () {
-  // 禁用双指放大
-  document.addEventListener('touchstart', function (event) {
-    if (event.touches.length > 1) {
-      event.preventDefault()
-    }
-  });
-  document.addEventListener('gesturestart', function (event) {
-    event.preventDefault()
-  })
-  // 禁用双击放大
-  var lastTouchEnd = 0;
-  document.documentElement.addEventListener('touchend', function (event) {
-    var now = Date.now();
-    if (now - lastTouchEnd <= 300) {
-      event.preventDefault();
-    }
-    lastTouchEnd = now;
-  }, {
-    passive: false
-  });
-};
+// onBeforeMount(() => {
+  // window.addEventListener("mousewheel", function (event) {
+  //   if (event.ctrlKey === true || event.metaKey) { event.preventDefault(); }
+  // }, { passive: false })
+
+  // window.onload = function () {
+  //   // 禁用双指放大
+  //   document.addEventListener('touchstart', function (event) {
+  //     if (event.touches.length > 1) {
+  //       event.preventDefault()
+  //     }
+  //   });
+  //   document.addEventListener('gesturestart', function (event) {
+  //     event.preventDefault()
+  //   })
+    // 禁用双击放大
+    // var lastTouchEnd = 0;
+    // document.documentElement.addEventListener('touchend', function (event) {
+    //   var now = Date.now();
+    //   if (now - lastTouchEnd <= 300) {
+    //     event.preventDefault();
+    //   }
+    //   lastTouchEnd = now;
+    // }, {
+    //   passive: false
+    // });
+  // };
+// })
 
 
 </script>
