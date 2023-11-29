cbcaeeb4929d78fcbd79b4a0d07f13e1155ca5e0
gshu <gshu@evomotion.com>
2023-11-29 18:57:29 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <script setup>
 import zhCn from "element-plus/es/locale/lang/zh-cn"
 
+window.onload = function () {
+  // 禁用双指放大
+  document.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+      event.preventDefault()
+    }
+  });
+  document.addEventListener('gesturestart', function (event) {
+    event.preventDefault()
+  })
+  // 禁用双击放大
+  var lastTouchEnd = 0;
+  document.documentElement.addEventListener('touchend', function (event) {
+    var now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  }, {
+    passive: false
+  });
+};
+
+
 </script>
 
 <template>
@@ -10,10 +34,10 @@ import zhCn from "element-plus/es/locale/lang/zh-cn"
 </template>
 
 <style>
-
 #app {
   height: 100%;
 }
+
 .el-popover {
   padding: 4px 0 !important;
   min-width: 100px !important;
@@ -23,6 +47,4 @@ import zhCn from "element-plus/es/locale/lang/zh-cn"
   padding: 4px 0 !important;
   min-width: 100px !important;
 }
-
-
 </style>
